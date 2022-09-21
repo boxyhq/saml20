d164d50e38b021b61e326837e62a186ae2feeac2
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-09-21 12:51:21 +0530

    added test cases

--- test/lib/metadata.spec.ts
+++ test/lib/metadata.spec.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import fs from 'fs';
 
 const samlMetadata = fs.readFileSync('./test/assets/mock-saml-metadata.xml').toString();
+const samlMetadata1 = fs.readFileSync('./test/assets/mock-saml-metadata1.xml').toString();
 
 describe('metadata.ts', function () {
   it('saml MetaData ok', async function () {
@@ -12,6 +13,17 @@ describe('metadata.ts', function () {
     expect(value.loginType).to.equal('idp');
     expect(value.sso.postUrl).to.equal('http://localhost:4000/api/saml/sso');
     expect(value.sso.redirectUrl).to.equal('http://localhost:4000/api/saml/sso');
+    expect(value.validTo).to.equal('Aug 12 10:27:20 3021 GMT');
+  });
+
+  it('saml MetaData ok with cert having BEGIN & END notations', async function () {
+    const value = await parseMetadata(samlMetadata1, {});
+    expect(value.entityID).to.equal('https://saml.example.com/entityid');
+    expect(value.thumbprint).to.equal('f9e424fe5fb3422db37859fe29b7f92f11af60a7');
+    expect(value.loginType).to.equal('idp');
+    expect(value.sso.postUrl).to.equal('http://localhost:4000/api/saml/sso');
+    expect(value.sso.redirectUrl).to.equal('http://localhost:4000/api/saml/sso');
+    expect(value.validTo).to.equal('Aug 12 10:27:20 3021 GMT');
   });
 
   it('saml Metadata validateNameIDFormat ok', async function () {
